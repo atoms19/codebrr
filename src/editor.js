1c0e967032244d764b38695408725cee79b03964
atoms19 <vishalvdevan@gmail.com>
2025-03-29 23:59:10 +0530

    formatting

--- src/editor.js
+++ src/editor.js
@@ -2,7 +2,7 @@ import { button, div, effect, h1, nav, state } from "dominity";
 
 import { basicSetup } from "codemirror";
 import { javascript } from "@codemirror/lang-javascript";
-import { keymap,EditorView } from "@codemirror/view";
+import { keymap, EditorView } from "@codemirror/view";
 import { indentWithTab } from "@codemirror/commands";
 import { syntaxHighlighting } from "@codemirror/language";
 import { HighlightStyle } from "@codemirror/language";
@@ -12,65 +12,61 @@ import { cpp } from "@codemirror/lang-cpp";
 import { python } from "@codemirror/lang-python";
 import { recents } from "./main";
 
+let definedStyle = HighlightStyle.define([
+   { tag: tags.keyword, color: "#C678DD", fontWeight: "bold" }, // Purple keywords
+   { tag: tags.comment, color: "#5C6370", fontStyle: "italic" }, // Gray comments
+   { tag: tags.string, color: "#98C379" }, // Green strings
+   { tag: tags.function(tags.variableName), color: "#61AFEF" }, // Blue function names
+   { tag: tags.number, color: "#D19A66" }, // Orange numbers
+   { tag: tags.operator, color: "#56B6C2" }, // Cyan operators
+   { tag: tags.punctuation, color: "#9A9B9C" } // Light gray punctuation
+]);
 
-let definedStyle=HighlightStyle.define([
-    { tag: tags.keyword, color: "#C678DD", fontWeight: "bold" }, // Purple keywords
-    { tag: tags.comment, color: "#5C6370", fontStyle: "italic" }, // Gray comments
-    { tag: tags.string, color: "#98C379" }, // Green strings
-    { tag: tags.function(tags.variableName), color: "#61AFEF" }, // Blue function names
-    { tag: tags.number, color: "#D19A66" }, // Orange numbers
-    { tag: tags.operator, color: "#56B6C2" }, // Cyan operators
-    { tag: tags.punctuation, color: "#9A9B9C" }, // Light gray punctuation
-])
-
-function chooseLang(fname){
-    if(fname.endsWith('.c') || fname.endsWith('.cpp')){
-        return cpp()
-    }else if(fname.endsWith('.js') || fname.endsWith('.ts')){
-        return javascript({
-            typescript:true
-        })
-    }else if(fname.endsWith('.py')){
-        return python()
-    }
-
+function chooseLang(fname) {
+   if (fname.endsWith(".c") || fname.endsWith(".cpp")) {
+      return cpp();
+   } else if (fname.endsWith(".js") || fname.endsWith(".ts")) {
+      return javascript({
+         typescript: true
+      });
+   } else if (fname.endsWith(".py")) {
+      return python();
+   }
 }
 
 export function Editor(router) {
    let ref = state("");
 
+   let fname = router?.queries?.file || "newfile.js";
+   let language = new Compartment();
 
+   let sourceCode;
+   let filefinder = recents.value.find((p) => p.fname == fname);
+   if (filefinder) sourceCode = filefinder.code;
+   else sourceCode = "";
 
-   let fname=router?.queries?.file || 'newfile.js'
-    let language=new Compartment
-
-    let sourceCode;
-    let filefinder=recents.value.find(p=>p.fname==fname)
-    if(filefinder) sourceCode=filefinder.code;
-    else sourceCode=''
-
-    let editor;
+   let editor;
    effect(() => {
-      editor=new EditorView({
-        doc:sourceCode,
+      editor = new EditorView({
+         doc: sourceCode,
 
-         extensions: [basicSetup,
-         language.of(chooseLang(fname)),
-         keymap.of([indentWithTab]),
-         syntaxHighlighting(definedStyle)
-
-        ],
-         parent: ref.value,
-
+         extensions: [
+            basicSetup,
+            language.of(chooseLang(fname)),
+            keymap.of([indentWithTab]),
+            syntaxHighlighting(definedStyle)
+         ],
+         parent: ref.value
       });
    });
 
-   let saves=setInterval(()=>{
-    if(!filefinder) clearInterval(saves)
-    filefinder.code=editor.state.doc.toString()
-   },5000)
+   let saves = setInterval(() => {
+      if (!filefinder) clearInterval(saves);
+      filefinder.code = editor.state.doc.toString();
+   }, 5000);
 
    return div(
-    nav({class:'editor-nav'},h1(fname||'unknown.ts'),button("run")),
-    div().giveRef(ref, true));
+      nav({ class: "editor-nav" }, h1(fname || "unknown.ts"), button("run")),
+      div().giveRef(ref, true)
+   );
 }
--- src/main.js
+++ src/main.js
@@ -1,77 +1,85 @@
-import PromptWindow from './_components/promptWindow'
-import { Editor } from './editor'
-import './style.css'
-import "/fileplus.svg"
+import PromptWindow from "./_components/promptWindow";
+import { Editor } from "./editor";
+import "./style.css";
+import "/fileplus.svg";
 
-import { button, div, DominityRouter, h1, img, li, nav, p, state, ul} from 'dominity'
+import {
+   button,
+   div,
+   DominityRouter,
+   h1,
+   img,
+   li,
+   p,
+   state,
+   ul
+} from "dominity";
 
-export let recents=state([{
-  fname:'linkedlist.c',
-  date:'19-02-2025',
-},{
-  fname:'arraySort.py',
-  date:'19-02-2025'
-},
-{
-  fname:'nig.ts',
-  date:'19-02-2025'
-}])
+export let recents = state([
+   {
+      fname: "linkedlist.c",
+      date: "19-02-2025"
+   },
+   {
+      fname: "arraySort.py",
+      date: "19-02-2025"
+   },
+   {
+      fname: "nig.ts",
+      date: "19-02-2025"
+   }
+]);
 
-function Home(r){
+function Home(r) {
+   return div(
+      h1("hello codebrr"),
+      p("code brr is an execturion platform for various programming languages"),
+      button("new file", img({ src: "fileplus.svg" })).on("click", () => {
+         let file = "";
 
-  return div(
-    h1("hello codebrr")
-    ,p('code brr is an execturion platform for various programming languages'),
-    button('new file',img({src:'fileplus.svg'})).on("click",()=>{
-      let file=''
+         PromptWindow({
+            title: "File Name",
+            descrption: "enter the name of the file along with its extension",
+            prompt: "example.js"
+         }).on("promptEntry", (e) => {
+            file = e.detail.value;
+            r.routeTo("/editor?file=" + file);
 
-      PromptWindow({
-        title:'File Name',
-        descrption:'enter the name of the file along with its extension',
-        prompt:'example.js'
-      }).on("promptEntry",(e)=>{
-      file=e.detail.value
-      r.routeTo('/editor?file='+file)
-        
-      recents.value=[{
-        fname:file,
-        date:new Intl.DateTimeFormat('en-GB').format(new Date()).replaceAll('/','-')
-      },...recents.value]
+            recents.value = [
+               {
+                  fname: file,
+                  date: new Intl.DateTimeFormat("en-GB")
+                     .format(new Date())
+                     .replaceAll("/", "-")
+               },
+               ...recents.value
+            ];
+         });
+      }),
+      history()
+   );
+}
 
+function history() {
+   return div(
+      h1("Recents-----"),
+      ul().forEvery(recents, (recent) => {
+         return router.Link(
+            { href: "/editor?file=" + recent.fname },
+            li({ class: "recent-item" }, div(recent.fname), div(recent.date))
+         );
       })
-        
-    })
-   , history()
-  )
-
+   );
 }
 
-
-
-function history(){
-  return(
-      div(
-          h1('Recents-----'),
-          ul().forEvery(recents,(recent)=>{
-            return router.Link({href:'/editor?file='+recent.fname},li({class:'recent-item'},div(
-              recent.fname
-            ),div(recent.date)))
-          })
-
-      )
-  )
-}
-
-let router=new DominityRouter() 
+let router = new DominityRouter();
 router.setRoutes({
-  "/":{
-    getComponent:Home
-  },
-  "/editor":{
-    getComponent:Editor
-  }
-})
-router.start(document.body)
+   "/": {
+      getComponent: Home
+   },
+   "/editor": {
+      getComponent: Editor
+   }
+});
+router.start(document.body);
 //Home()
-
-
